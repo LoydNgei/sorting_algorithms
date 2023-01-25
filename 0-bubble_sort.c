1d285cdcb5b13ba04a2956e74e8e77bae529c62e
loyd ngei <loydngei@gmail.com>
2023-01-25 13:53:14 +0300

    Bubble sort algo

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,11 +9,12 @@ void bubble_sort(int *array, size_t size)
 {
 	int temp;
 	size_t i, j, n;
+
 	n = size;
 
-	for (i = 0; i < n-1; i++)
+	for (i = 0; i < n - 1; i++)
 	{
-		for (j = 0; j < n-1-i; j++)
+		for (j = 0; j < n - 1 - i; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
